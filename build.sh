a405ade06020ff06894eaac403cf1bbf6a9b911e
devthejo <jo@surikat.pro>
2021-01-29 00:39:30 +0100

    wip

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-TAG=${1:-latest}
-docker build -t registry.gitlab.factory.social.gouv.fr/devthejo/webhook-k8s-ci:$TAG .
-docker push registry.gitlab.factory.social.gouv.fr/devthejo/webhook-k8s-ci:$TAG
