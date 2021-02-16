7d8e17663b237861eda0a525a42c9e45d0b5cb30
devthejo <jo@surikat.pro>
2021-02-16 03:35:39 +0100

    fix

--- opt/jobs/delete/create.sh
+++ opt/jobs/delete/create.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-export JOB_ID=${JOB_ID:-"$(uuidgen)"}
+export K8SCI_GID=${K8SCI_GID:-"$(uuidgen)"}
 export K8SCI_BRANCH=${K8SCI_BRANCH:-"$(echo $K8SCI_REF | cut -d / -f3-)"}
 
 cat $(dirname $0)/delete-job.yaml | gomplate | kubectl -n $K8SCI_K8S_NS create -f -
