eaced8681f82dbad697904d3baf532b53352419b
devthejo <jo@surikat.pro>
2021-02-16 13:24:28 +0100

    nanosecond instead of uuidgen

--- opt/jobs/push/create.sh
+++ opt/jobs/push/create.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-export K8SCI_GID=${K8SCI_GID:-"$(uuidgen)"}
+export K8SCI_GID=${K8SCI_GID:-"$(date +'%s%3N')"}
 export K8SCI_BRANCH=${K8SCI_BRANCH:-"$(echo $K8SCI_REF | cut -d / -f3-)"}
 
 cat $(dirname $0)/push-job.yaml | gomplate | kubectl -n $K8SCI_K8S_NS create -f -
