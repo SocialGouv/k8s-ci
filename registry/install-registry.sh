962318cf2f60ccde35817cf5d38def2229c3f960
devthejo <jo@surikat.pro>
2022-02-08 18:19:50 +0100

    chore(up): wip

--- registry/install-registry.sh
+++ registry/install-registry.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 REGISTRY_NS="registry"
-REGISTRY_HOST="registry-xxxxx.my-cool-project-domain.tld"
-K8S_JOBS_NS="ci-xxxxx"
+REGISTRY_HOST="registry.dev.fabrique.social.gouv.fr"
+K8S_JOBS_NS="kubeci-emjpm"
 CREDS_SECRET_NAME="registry-creds"
 
 REGISTRY_USER=$(kubectl -n $K8S_JOBS_NS get secret $CREDS_SECRET_NAME -ojsonpath='{.data.user}' 2>/dev/null | base64 --decode)
 REGISTRY_PASS=$(kubectl -n $K8S_JOBS_NS get secret $CREDS_SECRET_NAME -ojsonpath='{.data.pass}' 2>/dev/null | base64 --decode)
-if [ -z "$REGISTRY_PASS" ]; then
-  REGISTRY_USER=k8s
-  REGISTRY_PASS=$(openssl rand -base64 32)
-  kubectl -n $K8S_JOBS_NS create secret generic $CREDS_SECRET_NAME \
-    --from-literal=user=$REGISTRY_USER --from-literal=pass=$REGISTRY_PASS
-fi
+# if [ -z "$REGISTRY_PASS" ]; then
+#   REGISTRY_USER=k8s
+#   REGISTRY_PASS=$(openssl rand -base64 32)
+#   kubectl -n $K8S_JOBS_NS create secret generic $CREDS_SECRET_NAME \
+#     --from-literal=user=$REGISTRY_USER --from-literal=pass=$REGISTRY_PASS
+# fi
 
 HTPASSWD=$(htpasswd -Bbn $REGISTRY_USER $REGISTRY_PASS)
 
@@ -34,7 +34,7 @@ helm -n $REGISTRY_NS upgrade --install docker-registry stable/docker-registry \
 
 # https://stackoverflow.com/a/61078171/5338073
 # kubectl -n $REGISTRY_NS annotate --overwrite ingress docker-registry 'kubernetes.io/ingress.class:="nginx"'
-kubectl -n $REGISTRY_NS annotate --overwrite ingress docker-registry 'nginx.org/client-max-body-size="0"'
-kubectl -n $REGISTRY_NS annotate --overwrite ingress docker-registry 'nginx.ingress.kubernetes.io/proxy-body-size="0"'
-kubectl -n $REGISTRY_NS annotate --overwrite ingress docker-registry 'nginx.ingress.kubernetes.io/proxy-read-timeout="600"'
-kubectl -n $REGISTRY_NS annotate --overwrite ingress docker-registry 'nginx.ingress.kubernetes.io/proxy-send-timeout="600"'
+kubectl -n $REGISTRY_NS annotate --overwrite ingress docker-registry 'nginx.org/client-max-body-size=0'
+kubectl -n $REGISTRY_NS annotate --overwrite ingress docker-registry 'nginx.ingress.kubernetes.io/proxy-body-size=0'
+kubectl -n $REGISTRY_NS annotate --overwrite ingress docker-registry 'nginx.ingress.kubernetes.io/proxy-read-timeout=600'
+kubectl -n $REGISTRY_NS annotate --overwrite ingress docker-registry 'nginx.ingress.kubernetes.io/proxy-send-timeout=600'
